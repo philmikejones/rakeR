957d0eb7b2da27e028a97cb65fb56eddb074bcd8
Phil Mike Jones <philmikejones@gmail.com>
2016-09-16 13:45:50 +0100

    Fix prep-cakemap

--- data-raw/prep-cakemap.R
+++ data-raw/prep-cakemap.R
@@ -2,14 +2,16 @@
 
 # Cons
 cons <- readr::read_csv("inst/extdata/cakemap_cons.csv")
-colnames(cons)[1:6]   <- paste0("b_", colnames(cons)[1:6])
-colnames(cons)[7:12]  <- paste0("a_", colnames(cons)[7:12])
-colnames(cons)[13:14] <- paste0("c_", colnames(cons)[13:14])
-colnames(cons)[15:ncol(cons)] <- paste0("n_", colnames(cons)[15:ncol(cons)])
 colnames(cons) <- gsub("X", "", colnames(cons))
 colnames(cons) <- gsub("\\.", "_", colnames(cons))
+colnames(cons)[13:14] <- c("car_yes", "car_no")
+colnames(cons)[15:ncol(cons)] <- paste0("n_", colnames(cons)[15:ncol(cons)])
+colnames(cons)[ncol(cons)] <- "n_97"
 cons$code <- paste0("c", 1:nrow(cons))
-cons <- cons[, c(ncol(cons), 7:12, 1:6, 13:ncol(cons) - 1)]
+cons <- cons[, c(ncol(cons), 13:14, 15:24, 7:12, 1:6)]
+
+# population of nssec is 3 out compared to age/sex and car
+cons[1, 6] <- 2775
 
 save(cons, file = "data/cakemap_cons.RData", compress = "xz")
 
@@ -21,9 +23,16 @@ inds <- inds[, c(6, 2:5, 1)]
 
 inds$ageband4 <- gsub("-", "_", inds$ageband4)
 inds$NSSEC8 <- gsub("\\.", "_", inds$NSSEC8)
+inds$NSSEC8 <- paste0("n_", inds$NSSEC8)
 inds$Sex <- factor(inds$Sex, levels = 1:2,
                    labels = c("male", "female"))
 inds$Car <- factor(inds$Car, levels = 1:2,
-                   labels = c("yes", "no"))
+                   labels = c("car_yes", "car_no"))
+
+inds$ageband4[inds$Sex == "male"] <-
+  paste0("m", inds$ageband4[inds$Sex == "male"])
+inds$ageband4[inds$Sex == "female"] <-
+  paste0("f", inds$ageband4[inds$Sex == "female"])
+inds <- inds[, -3]
 
 save(inds, file = "data/cakemap_inds.RData", compress = "xz")
