852eaf41d88127470d10700d88946c174ea2101e
Phil Mike Jones <philmikejones@gmail.com>
2016-09-16 12:04:19 +0100

    lfs track RData cakemap examples

--- tests/testthat/test_weight.R
+++ tests/testthat/test_weight.R
@@ -1,23 +1,19 @@
 context("Test weight function")
 
 cons <- data.frame(
-
   "zone"  = letters[1:3],
   "a0_49" = c(8, 2, 7),
   "a_gt50" = c(4, 8, 4),
   "f"    = c(6, 6, 8),
   "m"    = c(6, 4, 3)
-
 )
 
 inds <- data.frame(
-
   "id"     = LETTERS[1:5],
   "age"    = c("a_gt50", "a_gt50", "a0_49", "a_gt50", "a0_49"),
   "sex"    = c("m", "m", "m", "f", "f"),
   "income" = c(2868, 2474, 2231, 3152, 2473),
   stringsAsFactors = FALSE
-
 )
 
 vars <- c("age", "sex")
@@ -57,3 +53,12 @@ book_wt <- as.vector(matrix(
 test_that("Weights match weights given in Robin's book (p. 26)", {
   expect_equal(weights, book_wt)
 })
+
+
+
+
+vars <- c("Car", "Sex", "NSSEC8", "ageband4")
+
+
+
+rakeR::weight(cons, inds, vars)
