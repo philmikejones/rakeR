7a0a1d7a5d7f992e28825d0c56c0a720adde5568
Phil Mike Jones <philmikejones@gmail.com>
2017-06-26 23:35:21 +0100

    replace tests with cake map data
    vars needed correcting

--- tests/testthat/test_weight.R
+++ tests/testthat/test_weight.R
@@ -1,22 +1,8 @@
 context("Test weight function")
 
-cons <- data.frame(
-  "zone"  = letters[1:3],
-  "a0_49" = c(8, 2, 7),
-  "a_gt50" = c(4, 8, 4),
-  "f"    = c(6, 6, 8),
-  "m"    = c(6, 4, 3)
-)
-
-inds <- data.frame(
-  "id"     = LETTERS[1:5],
-  "age"    = c("a_gt50", "a_gt50", "a0_49", "a_gt50", "a0_49"),
-  "sex"    = c("m", "m", "m", "f", "f"),
-  "income" = c(2868, 2474, 2231, 3152, 2473),
-  stringsAsFactors = FALSE
-)
-
-vars <- c("age", "sex")
+cons <- readr::read_csv("tests/cakemap_cons.csv")
+inds <- readr::read_csv("tests/cakemap_inds.csv")
+vars <- c("Car", "NSSEC8", "ageband4")
 
 weights <- weight(cons = cons, inds = inds, vars = vars)
 
