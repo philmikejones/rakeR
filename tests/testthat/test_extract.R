0fe8a9afd688f63153efd4798562b4f3f2e19c53
Phil Mike Jones <philmikejones@gmail.com>
2017-06-27 18:57:00 +0100

    update extract tests()

--- tests/testthat/test_extract.R
+++ tests/testthat/test_extract.R
@@ -1,4 +1,4 @@
-context("Test extract() function")
+context("Test extract() function rejects numeric variables")
 
 cons <- readr::read_csv("../cakemap_cons.csv")
 inds <- readr::read_csv("../cakemap_inds.csv")
@@ -8,19 +8,21 @@ inds$income <- sample(2000:4000, size = nrow(inds), replace = TRUE)
 weights <- weight(cons = cons, inds = inds, vars = vars, iterations = 10)
 
 test_that("extract() errors with a numeric variable", {
-  expect_error(rakeR::extract(weights = weights, inds = inds, id = "code"),
+  expect_error(extract(weights = weights, inds = inds, id = "code"),
                regexp = "cannot work with numeric")
 })
+
+
+context("Test extract() works when income in binned")
 
 inds[["income"]] <- cut(inds[["income"]],
                         breaks = 2,
                         labels = c("low", "high"),
                         include.lowest = TRUE)
-
-extd_weights <- extract_weights(weights, inds, "id")
+extd_weights <- extract(weights = weights, inds = inds, id = "code")
 
 test_that("Number of zones is correct", {
-  expect_equal(length(extd_weights$code), length(cons$zone))
+  expect_equal(length(extd_weights$code), length(cons$code))
 })
 
 test_that("Simulated population total matched real population total", {
