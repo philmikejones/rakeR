6aab163ec94f5648ae604497365bcb93e0c364d4
Phil Mike Jones <philmikejones@gmail.com>
2017-06-27 21:04:19 +0100

    update integerise() tests

--- tests/testthat/test_integerise.R
+++ tests/testthat/test_integerise.R
@@ -4,18 +4,17 @@ cons <- readr::read_csv("../cakemap_cons.csv")
 inds <- readr::read_csv("../cakemap_inds.csv")
 vars <- c("Car", "NSSEC8", "ageband4")
 
-weights <- weight(cons = cons, inds = inds, vars = vars, iterations = 10)
-weights_int <- integerise(weights)
+weights <- weight(cons = cons, inds = inds, vars = vars)
+weights_int <- integerise(weights, inds)
+
+test_that("Sum of input weights should equal number of integerised cases", {
+  expect_equal(sum(weights), nrow(weights_int))
+})
+
+test_that("correct number of zones in integerised data", {
+  expect_equal(length(unique(weights_int[["zone"]])), nrow(cons))
+})
 
 test_that("integerised weights should add up to cons population", {
-  expect_equal(sum(weights_int), sum(weights))
-  expect_equal(sum(weights_int), (sum(cons[, -1] / length(vars))))
-})
-
-test_that("integerised weights should have one column per zone", {
-  expect_equal(ncol(weights_int), nrow(cons))
-})
-
-test_that("integerised weights are integers", {
-  expect_true(all(apply(weights_int, 2, is.integer)))
+  expect_equal(nrow(weights_int), (sum(cons[, -1] / length(vars))))
 })
