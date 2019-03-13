f2b9028c9e13e6f6a2d30912b48dfb44ad4e1aab
Phil Mike Jones <philmikejones@gmail.com>
2019-03-13 10:54:10 +0000

    Rationalise rk_weight() tests

--- tests/testthat/test_rk_weight.R
+++ tests/testthat/test_rk_weight.R
@@ -1,4 +1,4 @@
-context("Test rk_weight() function produces correct output")
+context("Test rk_weight()")
 
 
 cons <- data.frame(
@@ -20,18 +20,54 @@ vars <- c("age", "sex")
 weights <- rk_weight(cons, inds, vars)
 
 
-test_that("Check cons and inds are data frames", {
+# Test inputs
+test_that("Error if cons is not a data frame", {
   cons_notdf <- unlist(cons)
-  inds_notdf <- unlist(inds)
   expect_error(rk_weight(cons_notdf, inds, vars), "cons is not a data frame")
+})
+
+test_that("Error if inds is not a data frame", {
+  inds_notdf <- unlist(inds)
   expect_error(rk_weight(cons, inds_notdf, vars), "inds is not a data frame")
 })
 
 test_that("Error if vars is not a vector", {
   vars <- function(x) {}
-  expect_error(rk_weight(cons, inds, vars = vars), "vector")
+  expect_error(rk_weight(cons, inds, vars = vars), "vars is not a vector")
 })
 
+test_that("Error if NAs present in cons", {
+  cons[1, 2] <- NA
+  expect_error(rk_weight(cons, inds, vars), "NA")
+})
+
+test_that("Error if NAs present in inds", {
+  inds[1, 2] <- NA
+  expect_error(rk_weight(cons, inds, vars), "NA")
+})
+
+test_that("Error if duplicate cons zone codes", {
+  cons[2, 1] <- cons[1, 1]
+  expect_error(rk_weight(cons, inds, vars), "duplicate zone codes")
+})
+
+test_that("Error if duplicate inds IDs", {
+  inds[2, 1] <- inds[1, 1]
+  expect_error(rk_weight(cons, inds, vars), "duplicate ids")
+})
+
+test_that("Error if any cons zones are 0", {
+  cons[1, 2:ncol(cons)] <- 0L
+  expect_error(rk_weight(cons, inds, vars), "contain 0")
+})
+
+test_that("Error if column names (ind/cons) don't match", {
+  inds$age[inds$age == "age_0_49"] <- "not_age_0_49"
+  expect_error(rk_weight(cons, inds, vars), "do not match")
+})
+
+
+# Test outputs
 test_that("Ncols should equal number of zones in cons", {
   expect_equal(ncol(weights), nrow(cons))
 })
@@ -52,61 +88,8 @@ test_that("No missing values", {
 
 test_that("Populations match (i.e. sum weights == (sum cons / n vars))", {
   expect_equal(sum(weights), (sum(cons[, -1]) / length(vars)))
-  # Drop first column because it contains zone numbers
 })
 
 test_that("individual IDs stored in rownames of weights", {
   expect_equal(rownames(weights), inds[[1]])
-})
-
-test_that("Check for data frame errors correctly", {
-  cons_notdf <- unlist(cons)
-  inds_notdf <- unlist(inds)
-  expect_error(rk_weight(cons_notdf, inds, vars), "cons is not a data frame")
-  expect_error(rk_weight(cons, inds_notdf, vars), "inds is not a data frame")
-})
-
-test_that("Check duplicate cons or inds IDs are picked up", {
-  expect_false(any(duplicated(cons[, 1])))
-  if (nrow(cons > 1)) {
-    cons[1, 1] <- cons[2, 1]  # duplicate zone id
-    expect_true(any(duplicated(cons[, 1])))
-  }
-
-})
-
-test_that("Errors if NAs present", {
-  # test inds first: if cons was first it would never get to inds
-  inds[1, 2] <- NA
-  expect_error(rk_weight(cons, inds, vars), "NA")
-
-  cons[1, 2] <- NA
-  expect_error(rk_weight(cons, inds, vars), "NA")
-})
-
-test_that("Duplicated zone codes produces an error", {
-  cons[2, 1] <- cons[1, 1]
-  expect_error(rk_weight(cons, inds, vars), "zone codes")
-})
-
-test_that("Duplicated ID codes produces an error", {
-  inds[2, 1] <- inds[1, 1]
-  expect_error(rk_weight(cons, inds, vars), "individual IDs")
-})
-
-test_that("Error if column names (ind/cons) don't match", {
-  inds$Car[inds$Car == "car_no"] <- "car_maybe"
-  expect_error(rk_weight(cons, inds, vars), "do not match")
-})
-
-test_that("Error if any zone completely empty", {
-  # Make one observation 0 to ensure this is being handled correctly
-  cons[19, "n_1_1"] <- 0  # lowest count (35)
-  cons[19, "n_1_2"] <- 283  # add these so populations still match
-  stopifnot(any(cons == 0))
-
-  cons[1, 2:ncol(cons)] <- 0
-  expect_error(
-    rk_weight(cons, inds, vars),
-    "0 population")
 })
